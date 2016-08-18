139b208874532c270495a00e6e42870a4667f7be
Sebastian Green-Husted <ricecake@tfm.nu>
2016-08-18 19:19:11 -0400

    add an app management framework

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 import "log"
+import "os"
+import "github.com/spf13/pflag"
 import "gopkg.in/olebedev/go-duktape.v2"
 import "github.com/boltdb/bolt"
+import "github.com/ricecake/asecdAgent/cmd"
 //import "github.com/gorilla/websocket"
 //import "github.com/ugorji/go/codec"
 //import "crypto/sha1"
@@ -19,4 +22,9 @@ func main() {
   result := ctx.GetNumber(-1)
   ctx.Pop()
   fmt.Println("result is:", result)
+
+    if err := cmd.RootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(-1)
+    }
 }
